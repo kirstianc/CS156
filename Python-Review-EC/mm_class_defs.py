546fad4b89116546d01fdba901f723f1c73f8b91
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-08-29 09:31:28 -0700

    setup for py ec

---
+++ Python-Review-EC/mm_class_defs.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+#NAME:  mm_class_defs.py
+#DESCRIPTION: contain class definitions that will be imported and used to create and
+#             manipulate Min-Max Tree objects 
+
+"""
+AUTHOR: Ian Chavez
+
+   Unpublished-rights reserved under the copyright laws of the United States.
+
+   This data and information is proprietary to, and a valuable trade secret
+   of, Leonard P. Wesley and Ian Chavez. It is given in confidence by Leonard
+   P. Wesley and Ian Chavez. Its use, duplication, or disclosure is subject to
+   the restrictions set forth in the License Agreement under which it has been
+   distributed.
+
+      Unpublished Copyright © 2022  Leonard P. Wesley and Ian Chavez
+      All Rights Reserved
+
+========================== MODIFICATION HISTORY ==============================
+08/29/23:
+    MOD:     Creation of file and initial organization
+    AUTHOR:  Ian Chavez
+    COMMENT: n/a
+====================== END OF MODIFICATION HISTORY ============================
+"""
+
+class MM_Node:
+    
