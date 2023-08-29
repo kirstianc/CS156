ec5ef61c8e6c8f88b6b3cf00b076c5f216e2bd5d
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-08-29 10:30:03 -0700

    class def updated until simplified instrct

--- Python-Review-EC/mm_class_defs.py
+++ Python-Review-EC/mm_class_defs.py
@@ -26,4 +26,78 @@ AUTHOR: Ian Chavez
 """
 
 class MM_Node:
+    """
+    slots/attributes --------------------------------
+    """
+    name
+    value
+    left_child
+    right_child 
+    parent
+    """
+    getters --------------------------------
+    """
+    def get_name(self):
+        return self.name
     
+    def get_value(self):
+        return self.value
+    
+    def get_parent(self):
+        return self.parent
+    
+    def get_left_child(self):
+        return self.left_child
+    
+    def get_right_child(self):
+        return self.right_child
+    
+    def get_children(self):
+        return (self.left_child, self.right_child)
+    """
+    setter functions --------------------------------
+    """
+    def set_name(self, name):
+        self.name = name
+
+    def set_value(self, value):
+        self.value = value
+
+    def set_parent(self, parent):
+        self.parent = parent
+    
+    def set_left_child(self, left_child):
+        self.left_child = left_child
+    
+    def set_right_child(self, right_child):
+        self.right_child = right_child
+
+    def set_children(self, left_child, right_child):
+        self.left_child = left_child
+        self.right_child = right_child
+    """
+    other functions --------------------------------
+    """
+    def __str__ (self):
+        return "NAME: " + str(self.name) + "\nVALUE: " + str(self.value) + "\nPARENT: " + str(self.parent) + "\nLEFT CHILD: " + str(self.left_child) + "\nRIGHT CHILD: " + str(self.right_child)
+
+    def __init__(self):
+        self.name = None
+        self.value = None
+        self.left_child = None
+        self.right_child = None
+        self.parent = None
+
+    def __init__(self, name):
+        self.name = name
+        self.value = None
+        self.left_child = None
+        self.right_child = None
+        self.parent = None
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+        self.parent = None
