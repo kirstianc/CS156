3f633110c997574d03edd7f1c65ad1063e4f8b49
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-08-28 22:56:32 -0700

    added cpr for basic functionality

--- Main/CPR.py
+++ Main/CPR.py
@@ -1,72 +1,95 @@
 # coding: utf-8
 #NAME:  CPR.py
-"""
-Brief description of what the module does or purpose. REQUIRED
-"""
+#DESCRIPTION: This will be a program to display all credit cards and due dates + recommended payment dates. 
+#             This is used to help me keep track of my credit cards and when I should pay them.
+#             This will also be used to review Python.
 
 """
-Modification history and info.   NOT REQUIRED 
-"""
-"""
-AUTHOR: Leonard P. Wesley
+AUTHOR: Ian Chavez
 
    Unpublished-rights reserved under the copyright laws of the United States.
 
    This data and information is proprietary to, and a valuable trade secret
-   of, Leonard P. Wesley and < add your name(s) >. It is given in confidence by Leonard
-   P. Wesley and < add your name(s)>. Its use, duplication, or disclosure is subject to
+   of, Leonard P. Wesley and Ian Chavez. It is given in confidence by Leonard
+   P. Wesley and Ian Chavez. Its use, duplication, or disclosure is subject to
    the restrictions set forth in the License Agreement under which it has been
    distributed.
 
-      Unpublished Copyright © 2022  Leonard P. Wesley and< add your name(s) >
+      Unpublished Copyright © 2022  Leonard P. Wesley and Ian Chavez
       All Rights Reserved
 
-  ============== VARIABLE, FUNCTION, etc. NAMING CONVENTIONS ==================
-<ALL CAPITOL LETTERS>:  Indicates a symbol defined by a
-        #define statement or a Macro.
-
-   <Capitalized Word>:  Indicates a user defined global var, fun, or typedef.
-
-   <all small letters>:  A variable or built in functions.
-
-
 ========================== MODIFICATION HISTORY ==============================
-The format for each modification entry is:
-
-MM/DD/YY:
-    MOD:     <a description of what was done>
-    AUTHOR:  <who made the mod>
-    COMMENT: <any special notes to make about the mod (e.g., what other
-              modules/code depends on the mod) >
-
-    Each entry is separated by the following line:
-
-====================== END OF MODIFICATION HISTORY ============================
-========================== MODIFICATION HISTORY ==============================
-10/23/22:
-    MOD:    Added gencode_fasta_dictionary_GRCh38 and msbi_fasta_data
-            to the set of arguments in the function call for the
-            epigen_pipeline_chromosome_snp_modification module
-    AUTHOR:  <NAME>
-    COMMENT: Appended gencode_fasta_dictionary_GRCh38 and msbi_fasta_data
-             to the arguments for epigen_pipeline_chromosome_snp_modification
+08/28/23:
+    MOD:     Creation of file and initial organization
+    AUTHOR:  Ian Chavez
+    COMMENT: n/a
 ====================== END OF MODIFICATION HISTORY ============================
 """
+import os
 
+credit_card_companies = {}
+
+def print_dict (input):
+    os.system('cls')
+    print("=====================================================================")
+    print("Companies :: Due Dates")
+    for key, value in input.items():
+        print(key, ":",value)
+    print("=====================================================================")
+
+def print_clr (input):
+    os.system('cls')
+    print("=====================================================================") 
+    print(input)
+    print("=====================================================================") 
+
+"""cannot Overload functions in Python..? -change to print_clr2"""
+def print_clr2 (input, input2): 
+    os.system('cls')
+    print("=====================================================================") 
+    print(input)
+    print(input2)
+    print("=====================================================================") 
+
+
+def add_cc (user_input):
+    if(user_input == "Y" or user_input == "y"):
+        print_clr("Please enter the name of the credit card company. (Example: Chase, Discover, etc.)")
+        credit_card_company = input()
+
+        print_clr("Please enter the due date of the credit card. (Example: 1, 15, 27, etc.)")
+        credit_card_due_date = input()
+
+        credit_card_companies[credit_card_company] = credit_card_due_date
+        print_clr("Credit card company and due date added. Do you have another credit card to add? (Y/N)")
+        user_input = input()
+        if(user_input == "Y" or user_input == "y"):
+            add_cc("y")
+        else:
+            print_clr("Thank you for using CPR.py. Goodbye.")
+
+def main ():
+    print_clr2("Hello welcome to CPR.py.", "Would you like to input a credit card and it's due date? (Y/N)") 
+    
+    user_input = input()
+    add_cc(user_input)
+
+    print_clr("Would you like to see what credit cards you have and their due dates? (Y/N)")
+    user_input = input()
+    if(user_input == "Y" or user_input == "y"):
+        print_dict(credit_card_companies)
+    else:
+        print_clr("Thank you for using CPR.py. Goodbye.")    
+
+    print("CPR.py:  Main function is executed.")
 
 """
-Imports
-"""
-import os                   # This is just an example import statement
-from copy import deepcopy   # This is another import example
-
-
-"""
-Module execute/load check.   REQUIRED
+Module execute/load check
 """
 if __name__ == '__main__':
-    print("cs156_pr1.py:  Module is executed.")
+    main()
+    print("CPR.py:  Module is executed.")
 
 else:
-    print("cs156_pr1.py:  Module is imported.")
+    print("CPR.py:  Module is imported.")
 
