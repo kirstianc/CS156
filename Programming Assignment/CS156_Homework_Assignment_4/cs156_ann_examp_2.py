5d0a6e33e37d2a02cfec6d847c95fdce4331c0ca
kirstianc <48222417+kirstianc@users.noreply.github.com>
2023-11-29 20:46:27 -0800

    hw4

--- Programming Assignment/CS156_Homework_Assignment_4/cs156_ann_examp_2.py
+++ Programming Assignment/CS156_Homework_Assignment_4/cs156_ann_examp_2.py
@@ -5,64 +5,59 @@ import os, sys
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
-#from neuralnet import NeuralNetMLP
+
+# from neuralnet import NeuralNetMLP
 from scipy.special import expit
 
 
 # Reads in data set and labels
-def load_mnist(path, kind='train'):
-	"""Load MNIST data from `path`"""
-	labels_path = os.path.join(path,
-		'%s-labels.idx1-ubyte'
-		% kind)
-	images_path = os.path.join(path,
-		'%s-images.idx3-ubyte'
-		% kind)
+def load_mnist(path, kind="train"):
+    """Load MNIST data from `path`"""
+    labels_path = os.path.join(path, "%s-labels.idx1-ubyte" % kind)
+    images_path = os.path.join(path, "%s-images.idx3-ubyte" % kind)
 
-	with open(labels_path, 'rb') as lbpath:
-		magic, n = struct.unpack('>II',
-			lbpath.read(8))
-		labels = np.fromfile(lbpath,
-			dtype=np.uint8)
+    with open(labels_path, "rb") as lbpath:
+        magic, n = struct.unpack(">II", lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
 
-	with open(images_path, 'rb') as imgpath:
-		magic, num, rows, cols = struct.unpack(">IIII",
-					imgpath.read(16))
+    with open(images_path, "rb") as imgpath:
+        magic, num, rows, cols = struct.unpack(">IIII", imgpath.read(16))
 
-		images = np.fromfile(imgpath,
-			dtype=np.uint8).reshape(len(labels), 784)
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
 
-	return images, labels
+    return images, labels
 
 
 # Lets read in the data set and labels
-#X_train, y_train = load_mnist('mnist', kind='train')
-X_train, y_train = load_mnist(os.getcwd(), kind='train')
+# X_train, y_train = load_mnist('mnist', kind='train')
+X_train, y_train = load_mnist(os.getcwd(), kind="train")
 print("Training data:")
-print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
+print("Rows: %d, columns: %d" % (X_train.shape[0], X_train.shape[1]))
 print()
 
 
-
-#X_test, y_test = load_mnist('mnist', kind='t10k')
-X_test, y_test = load_mnist(os.getcwd(), kind='t10k')
+# X_test, y_test = load_mnist('mnist', kind='t10k')
+X_test, y_test = load_mnist(os.getcwd(), kind="t10k")
 print("Test data:")
-print('Rows: %d, columns: %d' % (X_test.shape[0], X_test.shape[1]))
+print("Rows: %d, columns: %d" % (X_test.shape[0], X_test.shape[1]))
 print()
 
-fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True,)
+fig, ax = plt.subplots(
+    nrows=2,
+    ncols=5,
+    sharex=True,
+    sharey=True,
+)
 ax = ax.flatten()
 
 for i in range(10):
     img = X_train[y_train == i][0].reshape(28, 28)
-    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    ax[i].imshow(img, cmap="Greys", interpolation="nearest")
 
 ax[0].set_xticks([])
 ax[0].set_yticks([])
 plt.tight_layout()
 plt.show()
-
-
 
 
 # Save images as  .csv  files
@@ -101,7 +96,7 @@ We implement a 784-50-10 MLP, a neural network with 784 input units (n_features)
 
 
 class NeuralNetMLP(object):
-    """ Feedforward neural network / Multi-layer perceptron classifier.
+    """Feedforward neural network / Multi-layer perceptron classifier.
 
     Parameters
     ------------
@@ -156,11 +151,22 @@ class NeuralNetMLP(object):
       Sum of squared errors after each epoch.
 
     """
-    def __init__(self, n_output, n_features, n_hidden=30,
-                 l1=0.0, l2=0.0, epochs=500, eta=0.001,
-                 alpha=0.0, decrease_const=0.0, shuffle=True,
-                 minibatches=1, random_state=None):
 
+    def __init__(
+        self,
+        n_output,
+        n_features,
+        n_hidden=30,
+        l1=0.0,
+        l2=0.0,
+        epochs=500,
+        eta=0.001,
+        alpha=0.0,
+        decrease_const=0.0,
+        shuffle=True,
+        minibatches=1,
+        random_state=None,
+    ):
         np.random.seed(random_state)
         self.n_output = n_output
         self.n_features = n_features
@@ -178,6 +184,7 @@ class NeuralNetMLP(object):
     """
     A NN needs to work with numbers.
     """
+
     def _encode_labels(self, y, k):
         """Encode labels into one-hot representation
 
@@ -198,9 +205,9 @@ class NeuralNetMLP(object):
 
     def _initialize_weights(self):
         """Initialize weights with small random numbers."""
-        w1 = np.random.uniform(-1.0, 1.0, size=self.n_hidden*(self.n_features + 1))
+        w1 = np.random.uniform(-1.0, 1.0, size=self.n_hidden * (self.n_features + 1))
         w1 = w1.reshape(self.n_hidden, self.n_features + 1)
-        w2 = np.random.uniform(-1.0, 1.0, size=self.n_output*(self.n_hidden + 1))
+        w2 = np.random.uniform(-1.0, 1.0, size=self.n_output * (self.n_hidden + 1))
         w2 = w2.reshape(self.n_output, self.n_hidden + 1)
         return w1, w2
 
@@ -218,16 +225,16 @@ class NeuralNetMLP(object):
         sg = self._sigmoid(z)
         return sg * (1 - sg)
 
-    def _add_bias_unit(self, X, how='column'):
+    def _add_bias_unit(self, X, how="column"):
         """Add bias unit (column or row of 1s) to array at index 0"""
-        if how == 'column':
-            X_new = np.ones((X.shape[0], X.shape[1]+1))
+        if how == "column":
+            X_new = np.ones((X.shape[0], X.shape[1] + 1))
             X_new[:, 1:] = X
-        elif how == 'row':
-            X_new = np.ones((X.shape[0]+1, X.shape[1]))
+        elif how == "row":
+            X_new = np.ones((X.shape[0] + 1, X.shape[1]))
             X_new[1:, :] = X
         else:
-            raise AttributeError('`how` must be `column` or `row`')
+            raise AttributeError("`how` must be `column` or `row`")
         return X_new
 
     def _feedforward(self, X, w1, w2):
@@ -262,21 +269,27 @@ class NeuralNetMLP(object):
           Activation of output layer.
 
         """
-        a1 = self._add_bias_unit(X, how='column')
+        a1 = self._add_bias_unit(X, how="column")
         z2 = w1.dot(a1.T)
-        a2 = self._sigmoid(z2)
-        a2 = self._add_bias_unit(a2, how='row')
+        # a2 = self._sigmoid(z2)
+        a2 = self._hard_limit(z2)
+        a2 = self._add_bias_unit(a2, how="row")
         z3 = w2.dot(a2)
-        a3 = self._sigmoid(z3)
+        # a3 = self._sigmoid(z3)
+        a3 = self._hard_limit(z3)
         return a1, z2, a2, z3, a3
+
+    # added for HW4
+    def _hard_limit(self, z):
+        return np.where(z >= 0, 1, 0)
 
     def _L2_reg(self, lambda_, w1, w2):
         """Compute L2-regularization cost"""
-        return (lambda_/2.0) * (np.sum(w1[:, 1:] ** 2) + np.sum(w2[:, 1:] ** 2))
+        return (lambda_ / 2.0) * (np.sum(w1[:, 1:] ** 2) + np.sum(w2[:, 1:] ** 2))
 
     def _L1_reg(self, lambda_, w1, w2):
         """Compute L1-regularization cost"""
-        return (lambda_/2.0) * (np.abs(w1[:, 1:]).sum() + np.abs(w2[:, 1:]).sum())
+        return (lambda_ / 2.0) * (np.abs(w1[:, 1:]).sum() + np.abs(w2[:, 1:]).sum())
 
     def _get_cost(self, y_enc, output, w1, w2):
         """Compute cost function.
@@ -299,6 +312,14 @@ class NeuralNetMLP(object):
           Regularized cost.
 
         """
+        errors = y_enc - output
+        cost = np.mean(errors**2)
+        L1_term = self._L1_reg(self.l1, w1, w2)
+        L2_term = self._L2_reg(self.l2, w1, w2)
+        cost = cost + L1_term + L2_term
+        return cost
+        """
+        OLD CODE
         term1 = -y_enc * (np.log(output))
         term2 = (1 - y_enc) * np.log(1 - output)
         cost = np.sum(term1 - term2)
@@ -306,9 +327,10 @@ class NeuralNetMLP(object):
         L2_term = self._L2_reg(self.l2, w1, w2)
         cost = cost + L1_term + L2_term
         return cost
+        """
 
     def _get_gradient(self, a1, a2, a3, z2, y_enc, w1, w2):
-        """ Compute gradient step using backpropagation.
+        """Compute gradient step using backpropagation.
 
         Parameters
         ------------
@@ -345,7 +367,7 @@ class NeuralNetMLP(object):
         """
         # backpropagation
         sigma3 = a3 - y_enc
-        z2 = self._add_bias_unit(z2, how='row')
+        z2 = self._add_bias_unit(z2, how="row")
         # For In-Class Exercise, Create a  self._linear_gradient(z2)  function and
         # comment out the following line and replace with  sigma2 = w2.T.dot(sigma3) * self._linear_gradient(z2)
         sigma2 = w2.T.dot(sigma3) * self._sigmoid_gradient(z2)
@@ -354,8 +376,8 @@ class NeuralNetMLP(object):
         grad2 = sigma3.dot(a2.T)
 
         # regularize
-        grad1[:, 1:] += (w1[:, 1:] * (self.l1 + self.l2))
-        grad2[:, 1:] += (w2[:, 1:] * (self.l1 + self.l2))
+        grad1[:, 1:] += w1[:, 1:] * (self.l1 + self.l2)
+        grad2[:, 1:] += w2[:, 1:] * (self.l1 + self.l2)
 
         return grad1, grad2
 
@@ -374,16 +396,18 @@ class NeuralNetMLP(object):
 
         """
         if len(X.shape) != 2:
-            raise AttributeError('X must be a [n_samples, n_features] array.\n'
-                                 'Use X[:,None] for 1-feature classification,'
-                                 '\nor X[[i]] for 1-sample classification')
+            raise AttributeError(
+                "X must be a [n_samples, n_features] array.\n"
+                "Use X[:,None] for 1-feature classification,"
+                "\nor X[[i]] for 1-sample classification"
+            )
 
         a1, z2, a2, z3, a3 = self._feedforward(X, self.w1, self.w2)
         y_pred = np.argmax(z3, axis=0)
         return y_pred
 
     def fit(self, X, y, print_progress=False):
-        """ Learn weights from training data.
+        """Learn weights from training data.
 
         Parameters
         -----------
@@ -413,12 +437,11 @@ class NeuralNetMLP(object):
         delta_w2_prev = np.zeros(self.w2.shape)
 
         for i in range(self.epochs):
-
             # adaptive learning rate
-            self.eta /= (1 + self.decrease_const*i)
+            self.eta /= 1 + self.decrease_const * i
 
             if print_progress:
-                sys.stderr.write('\rEpoch: %d/%d ' % (i+1, self.epochs))
+                sys.stderr.write("\rEpoch: %d/%d " % (i + 1, self.epochs))
                 sys.stderr.flush()
 
             # Shuffle: Shuffling the training set prior to every epoch to
@@ -433,55 +456,65 @@ class NeuralNetMLP(object):
             # faster learning.
             mini = np.array_split(range(y_data.shape[0]), self.minibatches)
             for idx in mini:
-
-
                 # feedforward
                 # Train the MLP using 60,000 samples from the already shuffled MNIST training dataset.
                 a1, z2, a2, z3, a3 = self._feedforward(X_data[idx], self.w1, self.w2)
-                cost = self._get_cost(y_enc=y_enc[:, idx],
-                                      output=a3,
-                                      w1=self.w1,
-                                      w2=self.w2)
+                cost = self._get_cost(
+                    y_enc=y_enc[:, idx], output=a3, w1=self.w1, w2=self.w2
+                )
                 self.cost_.append(cost)
 
                 # compute gradient via backpropagation
-                grad1, grad2 = self._get_gradient(a1=a1, a2=a2,
-                                                  a3=a3, z2=z2,
-                                                  y_enc=y_enc[:, idx],
-                                                  w1=self.w1,
-                                                  w2=self.w2)
+                grad1, grad2 = self._get_gradient(
+                    a1=a1,
+                    a2=a2,
+                    a3=a3,
+                    z2=z2,
+                    y_enc=y_enc[:, idx],
+                    w1=self.w1,
+                    w2=self.w2,
+                )
 
                 delta_w1, delta_w2 = self.eta * grad1, self.eta * grad2
-                self.w1 -= (delta_w1 + (self.alpha * delta_w1_prev))
-                self.w2 -= (delta_w2 + (self.alpha * delta_w2_prev))
+                self.w1 -= delta_w1 + (self.alpha * delta_w1_prev)
+                self.w2 -= delta_w2 + (self.alpha * delta_w2_prev)
                 delta_w1_prev, delta_w2_prev = delta_w1, delta_w2
 
         return self
 
 
-
-
 print()
-print ("Creating NNMLP Object")
+print("Creating NNMLP Object")
 print()
 #  initialize a new 784-50-10 MLP, a neural network with 784 input units
 # (n_features), 50 hidden units (n_hidden), and 10 output units (n_output):
-#nn = NeuralNetMLP(n_output=10, n_features=X_train.shape[1], n_hidden=50, l2=0.1, l1=0.0, epochs=1000, eta=0.001, alpha=0.001, decrease_const=0.00001, shuffle=True, minibatches=50, random_state=1)
-nn = NeuralNetMLP(n_output=10, n_features=X_train.shape[1], n_hidden=50, l2=0.1, l1=0.0, epochs=50, eta=0.001, alpha=0.001, decrease_const=0.00001, shuffle=True, minibatches=50, random_state=1)
-
-
+# nn = NeuralNetMLP(n_output=10, n_features=X_train.shape[1], n_hidden=50, l2=0.1, l1=0.0, epochs=1000, eta=0.001, alpha=0.001, decrease_const=0.00001, shuffle=True, minibatches=50, random_state=1)
+nn = NeuralNetMLP(
+    n_output=10,
+    n_features=X_train.shape[1],
+    n_hidden=50,
+    l2=0.1,
+    l1=0.0,
+    epochs=50,
+    eta=0.001,
+    alpha=0.001,
+    decrease_const=0.00001,
+    shuffle=True,
+    minibatches=50,
+    random_state=1,
+)
 
 
 # Train our nn
 print()
-print ("Starting To Train NN")
+print("Starting To Train NN")
 print()
 nn.fit(X_train, y_train, print_progress=True)
 
 plt.plot(range(len(nn.cost_)), nn.cost_)
 plt.ylim([0, 2000])
-plt.ylabel('Cost')
-plt.xlabel('Epochs * 50')
+plt.ylabel("Cost")
+plt.xlabel("Epochs * 50")
 plt.tight_layout()
 plt.show()
 
@@ -498,36 +531,41 @@ averaging over the mini-batch intervals.
 batches = np.array_split(range(len(nn.cost_)), 1000)
 cost_ary = np.array(nn.cost_)
 cost_avgs = [np.mean(cost_ary[i]) for i in batches]
-plt.plot(range(len(cost_avgs)), cost_avgs, color='red')
+plt.plot(range(len(cost_avgs)), cost_avgs, color="red")
 plt.ylim([0, 2000])
-plt.ylabel('Cost')
-plt.xlabel('Epochs')
+plt.ylabel("Cost")
+plt.xlabel("Epochs")
 plt.tight_layout()
 plt.show()
 
 
 # Now evaluate performancs on training data set
 y_train_pred = nn.predict(X_train)
-acc = np.sum(y_train == y_train_pred, axis=0) / float (X_train.shape[0])
-print('Training accuracy: %.2f%%' % (acc * 100.))
+acc = np.sum(y_train == y_train_pred, axis=0) / float(X_train.shape[0])
+print("Training accuracy: %.2f%%" % (acc * 100.0))
 
 # Now evaluate performancs on test data set
 y_test_pred = nn.predict(X_test)
-acc = np.sum(y_test == y_test_pred, axis=0) / float (X_test.shape[0])
-print('Test accuracy: %.2f%%' % (acc * 100.))
+acc = np.sum(y_test == y_test_pred, axis=0) / float(X_test.shape[0])
+print("Test accuracy: %.2f%%" % (acc * 100.0))
 
 
 # Now, let's take a look at some of the images that our MLP struggles with:
 miscl_img = X_test[y_test != y_test_pred][:25]
 correct_lab = y_test[y_test != y_test_pred][:25]
-miscl_lab= y_test_pred[y_test != y_test_pred][:25]
-fig, ax = plt.subplots(nrows=5,ncols=5, sharex=True, sharey=True,)
+miscl_lab = y_test_pred[y_test != y_test_pred][:25]
+fig, ax = plt.subplots(
+    nrows=5,
+    ncols=5,
+    sharex=True,
+    sharey=True,
+)
 ax = ax.flatten()
 
 for i in range(25):
     img = miscl_img[i].reshape(28, 28)
-    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
-    ax[i].set_title('%d) t: %d p: %d' % (i+1, correct_lab[i], miscl_lab[i]))
+    ax[i].imshow(img, cmap="Greys", interpolation="nearest")
+    ax[i].set_title("%d) t: %d p: %d" % (i + 1, correct_lab[i], miscl_lab[i]))
 ax[0].set_xticks([])
 ax[0].set_yticks([])
 plt.tight_layout()
